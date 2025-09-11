ec2c723e480c2eb4ecce620755d730daac51294a
evert2208 <65311634+evert2208@users.noreply.github.com>
2025-09-11 00:50:00 -0500

    refactory: modificacion modulo cursos

--- frontend_react/src/cursos/routes/CursosRoutes.tsx
+++ frontend_react/src/cursos/routes/CursosRoutes.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom"
 import { Navbar } from "../../components/Navbar"
 import { Footer } from "../../components"
 import { Cursos } from "../pages/Cursos"
-import { Home } from "../pages/Home"
 
 
 export const CursosRoutes = () => {
